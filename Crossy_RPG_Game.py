d608942fc68068bf11d7b3a67715d1ebddad5036
robbie <robbieraya@gmail.com>
2021-02-09 17:27:55 -0700

    video 15 done

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -4,10 +4,32 @@
 
 import pygame
 
+pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
+SCREEN_TITLE = 'Crossy RPG'
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
+clock = pygame.time.Clock()
+TICK_RATE = 60
+is_game_over = False
 
+# Create window of specified size in white to display the game
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Set the game window color to white
 game_screen.fill(WHITE_COLOR)
+pygame.display.set_caption(SCREEN_TITLE)
+
+while not is_game_over:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_game_over = True
+    print(event)
+
+    pygame.display.update()
+    clock.tick(TICK_RATE)
+
+pygame.quit()
+quit()
