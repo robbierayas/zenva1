62175671034a18a9177a9c0c554e861fcc89e7a3
robbie <robbieraya@gmail.com>
2021-02-14 13:16:08 -0700

    video 16

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -1,6 +1,6 @@
-# Pygame development 1
-# Start the basic game set up
-# set up the display
+# Pygame development 3
+#draw objects to screen
+# load images into objects
 
 import pygame
 
@@ -21,15 +21,32 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
 pygame.display.set_caption(SCREEN_TITLE)
 
+# load images
+player_image = pygame.image.load('player.png')
+# scale the image up
+player_image = pygame.transform.scale(player_image, (50, 50))
 while not is_game_over:
 
+    # loop to get all the events
     for event in pygame.event.get():
+        # if we have a quit type evemt, then exit
         if event.type == pygame.QUIT:
             is_game_over = True
-    print(event)
+        print(event)
 
+    # Draw a rectangle
+    # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
+    #Draw a circle
+    # pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+
+    #draw player image
+    game_screen.blit(player_image, (375, 375))
+
+    #update all game graphics
     pygame.display.update()
+    #tick the clock to update everything in the game
     clock.tick(TICK_RATE)
 
+# quit pygame and the program
 pygame.quit()
 quit()
